887eccae83baf473741fbd3764d65e2e3dbc0952
Alan Pason <alan.pason132@gmail.com>
2023-06-18 12:36:19 +0200

    Feat: points and highscore are updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ import React, {useEffect, useState} from "react";
 function App() {
   const [characters, setCharactes] = useState(data.cards);
   const [actualCharacters, setActualCharacters] = useState(getRandomCharacters());
+  const [points, setPoints] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [isLose, setIsLose] = useState(false);
+
+  useEffect(() => {
+    if (points >= highScore) {
+      setHighScore(points);
+    }
+  }, [points]);
+
+  function resetGame() {
+    setCharactes((prevCharacters) =>
+      prevCharacters.map((hero) => {
+        return {...hero, chosen: false};
+      })
+    );
+    setPoints(0);
+    setIsLose(false);
+  }
 
   function getRandomCharacters() {
     const tempArray = [];
@@ -25,6 +44,9 @@ function App() {
 
   function changeIsChosen(id) {
     setCharactes((prevCharacters) => {
+      if (prevCharacters[id].chosen === true) {
+        setIsLose(true);
+      }
       return prevCharacters.map((hero) => {
         if (hero.id === id) {
           return {...hero, chosen: true};
@@ -34,6 +56,7 @@ function App() {
       });
     });
 
+    isLose ? resetGame() : setPoints((prevVal) => prevVal + 1);
     changeCards();
   }
 
@@ -43,7 +66,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header points={points} highScore={highScore} />
       <div className="cards-container">
         {actualCharacters.map((hero) => {
           return <Card key={hero.id} picture={hero.pictureDir} title={hero.title} handleClick={() => changeIsChosen(hero.id)} />;
--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,11 +1,11 @@
-function Header() {
+function Header(props) {
   return (
     <header className="header">
       <h1 className="game-title">Witcher Memory Card</h1>
-      <h2 className="game-rules">Remeber all cards you already pointed out, and make sure you won't pointed them out again</h2>
+      <h2 className="game-rules">emeber all cards you already pointed out, and make sure you won't pointed them out again</h2>
       <div className="statistics">
-        <p className="points">Points: 11</p>
-        <p className="high-score">High Score: 12</p>
+        <p className="points">Points: {props.points}</p>
+        <p className="high-score">High Score: {props.highScore}</p>
       </div>
     </header>
   );
